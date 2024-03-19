a92d4ed123a22f4e461354efebf232e139931ff6
rrsfira <rrachmashafira@gmail.com>
2024-03-19 20:27:06 +0700

    first commit

---
+++ game/Hit Me/3.js
@@ -0,0 +1,6 @@
+function redirectToNextPage() {
+  // Simulate a delay before redirecting
+  setTimeout(function () {
+    window.location.href = "../Hit Me/HITME/mole.html";
+  }, 3000); // Redirect after 3 seconds
+}
---
+++ game/catur.js
@@ -0,0 +1,6 @@
+function redirectToNextPage() {
+    // Simulate a delay before redirecting
+    setTimeout(function () {
+      window.location.href = "../game/catur/Chess.html";
+    }, 3000); // Redirect after 3 seconds
+  }
